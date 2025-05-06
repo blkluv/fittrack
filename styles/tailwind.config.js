d41a24476cda5f750f5b2197ade45fd21dbb966c
CosLynx AI <drixisgod@gmail.com>
2025-05-06 15:17:47 +0530

    generated file: `styles/tailwind.config.js` [base-template]

---
+++ styles/tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        emerald: '#2ecc71',
+        belize: '#3498db',
+        orange: '#f39c12',
+      },
+      fontFamily: {
+        montserrat: ['Montserrat, sans-serif'],
+        'open-sans': ['Open Sans, sans-serif'],
+      },
+      spacing: {
+        '4': '4px',
+        '8': '8px',
+        '12': '12px',
+        '16': '16px',
+        '20': '20px',
+        '24': '24px',
+        '28': '28px',
+        '32': '32px',
+        '40': '40px',
+        '48': '48px',
+        '56': '56px',
+        '64': '64px',
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+  ],
+}
